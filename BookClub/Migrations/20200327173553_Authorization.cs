cb80ba49b367fd20a6966f506779a7b74c56de54
Krista Rutz <rutzkri000@gmail.com>
2020-03-27 10:38:30 -0700

    add initial migrations

---
+++ BookClub/Migrations/20200327173553_Authorization.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookClub.Migrations
+{
+    public partial class Authorization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
