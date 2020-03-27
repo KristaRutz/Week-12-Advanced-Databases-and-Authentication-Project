79aa59216e30ae2f313057a0d3d37869e2385ed4
Krista Rutz <rutzkri000@gmail.com>
2020-03-27 10:18:26 -0700

    Include 6 DbSets for model tables

--- BookClub/Models/BookClubContext.cs
+++ BookClub/Models/BookClubContext.cs
@@ -5,6 +5,13 @@ namespace BookClub.Models
 {
   public class BookClubContext : IdentityDbContext<ApplicationUser>
   {
+    public virtual DbSet<Book> Books {get;set;}
+    public virtual DbSet<Author> Authors {get;set;}
+    public virtual DbSet<Genre> Genres {get;set;}
+    public DbSet<BookGenre> BookGenre {get;set;}
+    public DbSet<AuthorBook> AuthorBook {get;set;}
+    public DbSet<AuthorGenre> AuthorGenre {get;set;}
 
+    public BookClubContext(DbContextOptions options) : base(options){}
   }
 }
